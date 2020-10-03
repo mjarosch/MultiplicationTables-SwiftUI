4f6a8af6bb490b393d40e50191cd56442b426571
Mike <mike@mikejarosch.com>
2020-10-03 09:08:54 -0500

    Initial Commit

---
+++ MultiplicationTables-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationTables-SwiftUI
+//
+//  Created by Mike Jarosch on 10/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultiplicationTables-SwiftUI/MultiplicationTables_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTables_SwiftUIApp.swift
+//  MultiplicationTables-SwiftUI
+//
+//  Created by Mike Jarosch on 10/3/20.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTables_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
